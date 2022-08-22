ce3308111793c85d8f7720186bfd33b22f9a9e94
akhilsugunan77 <104181198+akhilsugunan77@users.noreply.github.com>
2022-08-22 13:42:23 +0530

    js

--- public/script/main.js
+++ public/script/main.js
@@ -5,5 +5,4 @@ const hamburgerMenu = document.getElementById("hamburger-menu-js");
 hamburgerBtn.addEventListener("click",function(){
     hamburgerBtn.classList.toggle("active");
     hamburgerMenu.classList.toggle("active");
-    console.log("clicked");
 })
